e8a358fd8f61c62a44566a66ae4005ba36b091e6
rishivr21 <rishikantver@gmail.com>
2026-03-14 23:47:26 +0530

    Fix urls.py for Task 6

--- onlinecourse/urls.py
+++ onlinecourse/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 from . import views
 
+app_name = 'onlinecourse'
+
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('submit/<int:course_id>/', views.submit, name='submit'),
-    path('exam_result/<int:course_id>/', views.show_exam_result, name='show_exam_result'),
+
+    path('submit/<int:course_id>/',
+         views.submit,
+         name='submit'),
+
+    path('course/<int:course_id>/submission/<int:submission_id>/result/',
+         views.show_exam_result,
+         name='show_exam_result'),
 ]
